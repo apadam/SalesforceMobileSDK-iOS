cc315f3f915ca62b0f1600f54589d67134dc4a33
Adam Eastham <adame@apadmi.com>
2021-09-10 09:10:47 +0100

    fixed syntax

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SalesforceMobileSDK-iOS",
-            dependencies: []),
-            path: "libs",
+            dependencies: [],
+            path: "libs"),
     ]
 )
