e46080c02922fa6fe128e3fd3c2171be846e096d
Adam Eastham <adame@apadmi.com>
2021-09-10 12:22:01 +0100

    amended target structure

--- Package.swift
+++ Package.swift
@@ -19,12 +19,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SalesforceMobileSDK-iOS",
-            dependencies: ["SalesforceSDKCommon",
-                           "SalesforceAnalytics",
-                           "SmartStore",
-                           "MobileSync"],
-            path: "SourcesSwift"),
+            name: "SalesforceSDKCore-iOS",
+            dependencies: [],
+            path: "libs"),
         .target(
             name: "SalesforceSDKCommon",
             dependencies: [],
